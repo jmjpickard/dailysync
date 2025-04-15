c194b3a53c884f29bd61d9266016f2aa3ead8fb1
Jack Pickard <jack@hertilityhealth.com>
2025-04-15 11:41:54 +0100

    more tweaks

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,17 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './styles/global.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./styles/global.css";
 
-// Make sure electronAPI is available
-declare global {
-  interface Window {
-    electronAPI: any
-  }
-}
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
